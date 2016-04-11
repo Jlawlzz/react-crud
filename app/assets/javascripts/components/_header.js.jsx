586351fd5828b33ca545c91c472989848b57d1f5
Jordan Lawler <jordan.lawler@colorado.edu>
2016-04-10 22:19:17 -0600

    initial commit

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,14 @@
+var Header = React.createClass({
+  render(){
+    return (
+      <div>
+        <div>
+          <h1>YELLOW!</h1>
+        </div>
+        <div>
+          <Body />
+        </div>
+      </div>
+    );
+  }
+});
